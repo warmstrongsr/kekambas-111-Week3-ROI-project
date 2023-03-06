d9c1240d66fe651aa43ad7c2a288a35357c2283b
Will Armstrong Sr <107271171+warmstrongsr@users.noreply.github.com>
2023-03-06 09:11:31 -0600

    comment adds and tweaks

--- rentalprop.py
+++ rentalprop.py
@@ -1,10 +1,10 @@
 class PropertyROI:
+    # __init__
     def __init__(self):
-    
         self.monthly_income = 0
         self.expenses = {
         'water':0,'HOA':0, 'lawn':0, 'vacancy':0, 'repairs':0, 'CapEx':0, 'property_management':0,'mortgage':0, 'misc_fees':0 } 
-
+    # funtion to get all input *get_information*
     def get_information(self):
         self.monthly_income = float(input("Enter monthly income: "))
         for expense in self.expenses:
@@ -13,13 +13,13 @@ class PropertyROI:
         self.expenses['insurance'] = float(input("Enter insurance expense total: $"))
         self.expenses['misc_fees'] = (float(input("Enter potential fees total: $")))
         self.total_investment = sum([
-            float(input("Enter down payment: ")),
-            float(input("Enter closing cost: ")),
-            float(input("Enter rehab budget: "))
+            float(input("Enter down payment: $")),
+            float(input("Enter closing cost: $")),
+            float(input("Enter rehab budget: $"))
         ])
-
+    # calculation function
     def calculate_roi(self):
-        annual_cash_flow = (self.monthly_income * 12) - sum(self.expenses.values())* 12
+        annual_cash_flow = (self.monthly_income * 12) - sum(self.expenses.values()) * 12
         print(f"Monthly Income: {self.monthly_income}")
 
         print(f"Annual cash flow: {annual_cash_flow:.2f}")
@@ -29,16 +29,21 @@ class PropertyROI:
         return roi
         
         
-    
+    # "reciept" summation function
     def generate_receipt(self, roi):
         print("==== RENTAL PROPERTY RECEIPT ====")
-        print(f"Monthly Income: ${self.monthly_income}")
+        print("\n**********************************")
+        print(f"     Monthly Income: ${self.monthly_income}")
+        print("**********************************")
         print("\nExpenses:")
         for expense in self.expenses:
             print(f"{expense}: ${self.expenses[expense]}")
-        print(f"Total Investment: ${self.total_investment}")
-        print(f"\nReturn on Investment: {100*roi:.2f}%")
-        print("==== RENTAL PROPERTY RECEIPT ====")
+        print(f"\n   Total Investment: ${self.total_investment}")
+        print("\n**********************************")
+        print(f"    Return on Investment: {100*roi:.2f}%")
+        print("**********************************")
+        print("\n==== RENTAL PROPERTY RECEIPT ====")
+
 calculator = PropertyROI()
 calculator.get_information()
 roi = calculator.calculate_roi()
